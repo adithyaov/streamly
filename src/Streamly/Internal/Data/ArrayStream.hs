d85f4f00b49ac210a3e8b06c713564147f3c4302
Harendra Kumar <harendra.kumar@gmail.com>
2019-09-21 18:50:13 +0530

    import the renamed ArrayStream module

--- src/Streamly/Internal/Data/ArrayStream.hs
+++ src/Streamly/Internal/Data/ArrayStream.hs
@@ -8,7 +8,7 @@
 #include "inline.hs"
 
 -- |
--- Module      : Streamly.Memory.ArrayStream
+-- Module      : Streamly.Internal.Data.ArrayStream
 -- Copyright   : (c) 2019 Composewell Technologies
 --
 -- License     : BSD3
@@ -18,7 +18,7 @@
 --
 -- Combinators to efficiently manipulate streams of arrays.
 --
-module Streamly.Memory.ArrayStream
+module Streamly.Internal.Data.ArrayStream
     (
     -- * Creation
       arraysOf
